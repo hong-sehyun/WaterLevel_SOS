ec0de2a669e9da0d6208a45660f8345a26d35552
hong-sehyun <sese0733@naver.com>
2023-08-28 11:13:26 +0900

    화면 구성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Components/Main/Main';
+
 import Main1 from './Components/Main/Main1';
-import Main2 from './Components/Main/Main2';  // Assuming you have this component
-import Main3 from './Components/Main/Main3';  // Assuming you have this component
+import Main2 from './Components/Main/Main2';  
+import Main3 from './Components/Main/Main3';  
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 
@@ -17,7 +19,7 @@ function App() {
           <Route path="/main1" element={<Main1 />} />
           <Route path="/main2" element={<Main2 />} />
           <Route path="/main3" element={<Main3 />} />
-          <Route path="/" element={<Main1 />} />  
+          <Route path="/" element={<Main />} />  
         </Routes>
         <Footer />
       </>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './navbar.css';
 function Navbar() {
   return (
     <nav>
       <Link to="/main1">Main 1</Link>
-      <Link to="/main2">Main 2</Link>  {/* Add these lines */}
-      <Link to="/main3">Main 3</Link>  {/* Add these lines */}
+      <Link to="/main2">Main 2</Link>
+      <Link to="/main3">Main 3</Link>
     </nav>
   );
 }
