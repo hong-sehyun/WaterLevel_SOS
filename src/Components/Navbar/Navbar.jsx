d4c574b98d9efb5c036b8b68f0b725e0dd20465b
hong-sehyun <sese0733@naver.com>
2023-08-28 15:34:28 +0900

    Navbar 링크 달기

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdNightShelter, MdOutlineWater } from 'react-icons/md'
+import { LuNavigationOff } from 'react-icons/lu'
 import './navbar.css';
 function Navbar() {
   return (
     <nav>
-      <Link to="/main1">Main 1</Link>
-      <Link to="/main2">Main 2</Link>
-      <Link to="/main3">Main 3</Link>
+      <ul>
+        <li><strong>하천 범람 SOS</strong></li>
+      </ul>
+      <ul>
+        <li><Link to="/main1"><LuNavigationOff /> 통제(예정) 현황</Link></li>
+        <li><Link to="/main2"><MdOutlineWater /> 수위 예측</Link></li>
+        <li><Link to="/main3"><MdNightShelter/> 대피소</Link></li>
+      </ul>      
     </nav>
   );
 }
