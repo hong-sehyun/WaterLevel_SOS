3376b73f6cb782f8a30ac696a1b27709d2cf2ac1
hong-sehyun <sese0733@naver.com>
2023-08-31 10:01:08 +0900

    navbar 디자인 수정

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,16 +5,6 @@ import { LuNavigationOff } from 'react-icons/lu'
 import './navbar.css';
 function Navbar() {
   return (
-    // <nav>
-    //   <ul>
-    //     <li><strong>하천 범람 SOS</strong></li>
-    //   </ul>
-    //   <ul>
-    //     <li><Link to="/main1"><LuNavigationOff /> 통제(예정) 현황</Link></li>
-    //     <li><Link to="/main2"><MdOutlineWater /> 수위 예측</Link></li>
-    //     <li><Link to="/main3"><MdNightShelter/> 대피소</Link></li>
-    //   </ul>      
-    // </nav>
     <>
       <div className="nav">
       <h2 className='title'>
