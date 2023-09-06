b3639fd83c34e7078bff80c3cb97821f85e44987
hong-sehyun <sese0733@naver.com>
2023-09-06 17:01:35 +0900

    feat: 차트 구현
    웹소켓으로 받아온 JSON 형태의 수위데이터를 차트로 표현

--- src/Components/Main/WLpred.jsx
+++ src/Components/Main/WLpred.jsx
@@ -1,15 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Bar } from 'react-chartjs-2';
+import { Chart, LineController, LineElement, PointElement, CategoryScale, LinearScale, registerables } from 'chart.js';
+Chart.register(LineController, LineElement, PointElement, CategoryScale, LinearScale, ...registerables);
 const WLpred = () => {
+  const [wl, setWl] = useState(null);
 
+  useEffect(() => {
+    const ws = new WebSocket('ws://10.125.121.184:8080/pushservice');
+
+    ws.onopen = () => {
+      console.log('연결됨');
+    }
+
+    ws.onmessage = (msg) => {
+      const data = JSON.parse(msg.data);
+      console.log(data);
+      setWl(data);
+    }
+
+    ws.onclose = () => {
+      console.log('연결 끊어짐');
+    };
+    
+    ws.onerror = (error) => {
+      console.error(error);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [])
+
+
+  const generateTimeLabels = (start, count) => {
+    let timeLabels = [];
+    let currentHour = parseInt(start.slice(-4,-2), 10);
+    let currentDay = parseInt(start.slice(0,8), 10);
+
+    for (let i = 0; i < count; i++) {
+      timeLabels.push(`${String(currentDay).padStart(2, '0')}${String(currentHour).padStart(2, '0')}`);
+      currentHour += 1;
+
+      if (currentHour === 24) {
+        currentHour = 0;
+        currentDay += 1;
+      }
+    }
+
+    return timeLabels;
+  };
+
+  const data = wl && {
+    labels: [
+      ...generateTimeLabels(wl.pastStart, 6),
+      ...generateTimeLabels(wl.predStart, wl.list.length - 6)
+    ],
+    datasets: [{
+      label: 'Water Level (EL.m)',
+      data: wl.list,
+      backgroundColor: 'rgba(75, 192, 192, 0.5)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1
+    }]
+  };
 
   return (
     <>
-      <h1>
-        수위예측
-      </h1>
-      <Link to='/'>범람알림 받으러 가기</Link>
+      <h1>수위예측</h1>
+      {/* <div>
+        {wl && (
+          <>
+            <h2>Past Start: {wl.pastStart}</h2>
+            <h2>Pred Start: {wl.predStart}</h2>
+            <ul>
+              {wl.list.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div> */}
+       {wl && (
+        <>
+          <Bar 
+            data={data}
+            options={{
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  title: {
+                    display: true,
+                    text: 'Water Level (EL.m)'
+                  }
+                },
+                x: {
+                  title: {
+                    display: true,
+                    text: 'Time'
+                  }
+                }
+              }
+            }}
+          />
+        </>
+      )}
+      <Link to='/'>범람알림 받으러 가기 </Link>
     </>
   );
 }
