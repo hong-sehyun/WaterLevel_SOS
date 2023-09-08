4fe1880aa15cb56e525e2ad4475016fde5f332e8
hong-sehyun <sese0733@naver.com>
2023-09-08 17:46:55 +0900

    feat: 카카오 주소찾기 api
    카카오 주소찾기 api를 사용해 대피소 위치 등록

---
+++ src/Components/Admin/Register/RegiShelter.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
+import axios from 'axios';
+
+
+const RegiShelter = () => {
+    const [address, setAddress] = useState('');
+    const [name, setName] = useState('');
+
+
+
+    const onComplete = (data) => {
+        let fullAddress = data.address;
+        let extraAddress = '';
+
+        if (data.addressType === 'R') {
+            if (data.bname !== '') {
+                extraAddress += data.bname;
+            }
+            if (data.buildingName !== '') {
+                extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+            }
+            fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+
+        setAddress(fullAddress);
+    }
+
+    const open = useDaumPostcodePopup(onComplete);
+
+    const handleClick = () => {
+        // 주소검색 후 주소 클릭 시 해당 함수 수행
+        open({ onComplete: onComplete });
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://10.125.121.184:8080/shelter')
+        } catch (error) {
+
+        }
+
+    }
+
+
+
+    return (
+        <div>
+            <details>
+                <summary role="button" class="secondary">대피소 등록</summary>
+                <form onSubmit={handleSubmit}>
+                    <div className="grid">
+
+                        <input
+                            type="text"
+                            value={address}
+                            placeholder="주소 검색"
+                            onClick={handleClick}
+                            readOnly
+                        />
+                        <button class="secondary outline" onClick={handleClick}>주소 검색</button>
+                    </div>
+                    <input type='text' value={name} placeholder="대피소명 입력"/>
+                    <button type='suubmit'>등록하기</button>
+                </form>
+            </details>
+        </div>
+    );
+}
+
+export default RegiShelter
--- src/Components/Admin/Register/Register.jsx
+++ src/Components/Admin/Register/Register.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import RegiShelter from './RegiShelter.jsx'
 
 const Reigster = () => {
   return (
     <>
-    <h2>등록</h2>
-    <Link to='/register/location'>통제 구역 등록</Link>
+      <h2>등록</h2>
+      <Link to='/register/location'>통제 구역 등록</Link>
+      <RegiShelter />
+      
 
     </>
 
