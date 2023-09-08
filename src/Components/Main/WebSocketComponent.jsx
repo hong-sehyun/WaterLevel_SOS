bdd0a417f1e848a4e72045aea993d1e772dcdeeb
hong-sehyun <sese0733@naver.com>
2023-09-08 16:36:38 +0900

    chore: time format 변경
    백엔드로부터 받아오는 json 데이터 타입이 변경되어, formatTime 함수 변경

--- src/Components/Main/WebSocketComponent.jsx
+++ src/Components/Main/WebSocketComponent.jsx
@@ -1,49 +1,58 @@
 import React, { useEffect, useState } from 'react';
 
-const WebSocketComponent  = () => {
-    const [time, setTime] = useState('');
+const WebSocketComponent = () => {
+  const [time, setTime] = useState('');
 
-    function formatTime(data) {
-      const year = data.slice(1, 5);
-      const month = data.slice(5, 7);
-      const day = data.slice(7, 9);
-      const hour = data.slice(9, 11);
-      const minute = data.slice(11, 13);
-    
-      return `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
-    }
-  
-    useEffect(() => {
-      const ws = new WebSocket('ws://10.125.121.184:8080/pushservice');
-  
-      ws.onopen = () => {
-        console.log('연결됨');
-      };
-  
-      ws.onmessage = (msg) => {
-        console.log(msg.data);
-        const formattedTime = formatTime(msg.data);
-        setTime(formattedTime);
-      };
-  
-      ws.onclose = () => {
-        console.log('연결 끊어짐');
-      };
-  
-      ws.onerror = (error) => {
-        console.error(error);
-      };
-  
-      return () => {
-        ws.close();
-      };
-    }, []);
-  
-    return (
-      <div>
-        {time} 기준
-      </div>
-    );
+  function formatTime(data) {
+    // const year = data.slice(1, 5);
+    // const month = data.slice(5, 7);
+    // const day = data.slice(7, 9);
+    // const hour = data.slice(9, 11);
+    // const minute = data.slice(11, 13);
+    const year = data.substring(0, 4);
+    const month = data.substring(4, 6);
+    const day = data.substring(6, 8);
+    const hour = data.substring(8, 10);
+    const minute = data.substring(10, 12);
+
+    return `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://10.125.121.184:8080/pushservice');
+
+    ws.onopen = () => {
+      console.log('연결됨');
+    };
+
+    ws.onmessage = (e) => {
+      // console.log(e.data);
+      // const formattedTime = formatTime(e.data);
+      // setTime(formattedTime);
+      const data = JSON.parse(e.data);
+      const reqTime = formatTime(data.reqTime);
+      setTime(reqTime);
+
+    };
+
+    ws.onclose = () => {
+      console.log('연결 끊어짐');
+    };
+
+    ws.onerror = (error) => {
+      console.error(error);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      {time} 기준
+    </div>
+  );
 }
 
 export default WebSocketComponent 
