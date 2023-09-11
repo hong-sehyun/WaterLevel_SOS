252b4f9ff5bb94e44ad759d6126fc30b373f1e4e
hong-sehyun <sese0733@naver.com>
2023-09-11 14:27:27 +0900

    fix: Uncaught SyntaxError: Unexpected token '<'
    RegiShelter.jsx의 Uncaught SyntaxError: Unexpected token '<' 에러 수정

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.get('/fetchData', async (req, res) => {
     const { tm1, tm2, stn, disp, help, authKey } = req.query;
@@ -19,6 +23,10 @@ app.get('/fetchData', async (req, res) => {
         console.error("Error fetching data from the API:", error);
         res.status(500).json({ message: "Error fetching data from the API" });
     }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import reportWebVitals from './reportWebVitals';
 // import './tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// Uncaught SyntaxError: Unexpected token '<' 에러 수정
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
