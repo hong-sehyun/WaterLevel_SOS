b3dac0d41e178e325893e986befaa2386ca8f465
hong-sehyun <sese0733@naver.com>
2023-09-19 15:28:47 +0900

    Chore: 바로가기 버튼 추가
    범람알림 받으러 가기 버튼 추가

--- src/Components/Main/MainBT.jsx
+++ src/Components/Main/MainBT.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MainBT = () => {
     return (
         <div>
-            <a href="#" role="button" class="secondary outline">범람알림 받으러 가기</a>
+            <a href="/" role="button" class="secondary outline">범람알림 받으러 가기</a>
         </div>
     )
 }
