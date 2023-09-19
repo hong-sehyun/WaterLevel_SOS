465567ea3e7411ddf2fef652676c5728bcf2f6aa
hong-sehyun <sese0733@naver.com>
2023-09-19 15:19:16 +0900

    Remove: 불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
 import React from 'react';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/Main/Main';
+// import Home from './Components/Main/Home';
 import CtrlArea from './Components/Main/CtrlArea';
-import WLpred from './Components/Main/WLpred';  
-import Shelter from './Components/Main/Shelter';  
+import WLpred from './Components/Main/WLpred';
+import Shelter from './Components/Main/Shelter';
 import Footer from './Components/Footer/Footer';
 import Login from './Components/Admin/Login';
 
-import Register from './Components/Admin/Setting/Register';
 import RegiLoc from './Components/Admin/Setting/RegiLoc';
-import RegiList from './Components/Admin/Setting/RegiList';
-import MemberList from './Components/Admin/Setting/MemberList';
 import ShelterList from './Components/Admin/Setting/ShelterList';
+import MemberList from './Components/Admin/Setting/MemberList';
+import AlarmList from './Components/Admin/Setting/AlarmList';
 import Setting from './Components/Admin/Setting/Setting';
 import RegiShelter from './Components/Admin/Setting/RegiShelter';
 
 
-import Join from './Components/Admin/Join';
-
-
-
 function App() {
   return (
-    
-    <BrowserRouter>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/ctrlarea" element={<CtrlArea />} />
-          <Route path="/wlpred" element={<WLpred />} />
-          <Route path="/shelter" element={<Shelter />} />
-          <Route path="/" element={<Main />} />  
-          <Route path="/login" element={<Login />} />  
+    <div className='app-container'>
 
-          <Route path="/register" element={<Register />} />  
-          <Route path="/register/location" element={<RegiLoc />} /> 
+      <BrowserRouter>
+        <>
+          <Navbar />
+          <div className='content'>
 
-          {/* <Route path="/regilist" element={<RegiList />} />  
-          <Route path="/regilist/memberList" element={<MemberList />} /> 
-          <Route path="/regilist/shelterList" element={<ShelterList />} />  */}
+            <Routes>
+              <Route path="/ctrlarea" element={<CtrlArea />} />
+              <Route path="/wlpred" element={<WLpred />} />
+              <Route path="/shelter" element={<Shelter />} />
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<Login />} />
 
-
-          <Route path='/setting' element={<Setting/>} />
-          <Route path="/setting/memberList" element={<MemberList />} /> 
-          <Route path="/setting/shelterList" element={<ShelterList />} /> 
-          <Route path="/setting/regiShelter" element={<RegiShelter />} /> 
+              {/* <Route path="/register" element={<Register />} />
+              <Route path="/register/location" element={<RegiLoc />} /> */}
 
 
-          <Route path="/join" element={<Join />} />  
-          
+              <Route path='/setting' element={<Setting />} />
+              <Route path="/setting/memberList" element={<MemberList />} />
+              <Route path="/setting/shelterList" element={<ShelterList />} />
+              <Route path="/setting/regiShelter" element={<RegiShelter />} />
+              <Route path="/setting/alarmList" element={<AlarmList />} />
 
-        </Routes>
-        <Footer />
-      </>
-    </BrowserRouter>
+            </Routes>
+          </div>
+          <Footer />
+        </>
+      </BrowserRouter>
+    </div>
   );
 }
 
