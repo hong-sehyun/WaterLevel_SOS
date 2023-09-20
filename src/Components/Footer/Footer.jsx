0c04b2d163b0648b31d6289bab7a740d12215ab5
hong-sehyun <sese0733@naver.com>
2023-09-20 13:32:30 +0900

    Remove: 주석 삭제

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
       <img src={logo} alt='logo'/>
       <div className="contact">
         <div>(46241) 부산시 금정구 부산대학로63번길2 과학기술연구동 401호</div>
-        {/* <a href='https://elastic-game-f85.notion.site/f2dc1ffe37f64470b9ee28c620044968'><img src={notionIcon}/> 팀노션</a> */}
       </div>
     </div>
   )
