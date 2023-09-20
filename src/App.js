50dedef5e3ee9906c77fa98ea2692fe0f1e378c2
hong-sehyun <sese0733@naver.com>
2023-09-20 13:31:34 +0900

    Remove: 불필요한 파일 및 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/Main/Main';
